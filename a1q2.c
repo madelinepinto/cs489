760017668b7a9e15dff910f39e2a236f924deb4b
Madeline Pinto <72671428+madelinepinto@users.noreply.github.com>
2025-02-20 15:50:24 -0500

    Update a1q2.c

--- a1q2.c
+++ a1q2.c
@@ -1,16 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
-    
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
+#include <stdlib.h>
+
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        fprintf(stderr, "Error: Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+    
+    FILE* file = fopen(argv[1], "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file '%s'.\n", argv[1]);
+        return -1;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    
+    fclose(file);
+
+    printf("The size of the file is %ld bytes.\n", file_size);
+    
+    return 0;
 }
